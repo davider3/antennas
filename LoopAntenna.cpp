42879ef817f10c8bc35532689953bf2f08e764ca
David <dreinhardt389@gmail.com>
2024-09-27 18:29:57 +0100

    multiple antenna support

--- LoopAntenna.cpp
+++ LoopAntenna.cpp
@@ -19,12 +19,44 @@ LoopAntenna::LoopAntenna(MCP6S2x* ampA, MCP6S2x* ampB,
 }
 
 
+/**
+ * TODO: docstring
+ */
+LoopAntenna::GainInfo LoopAntenna::tree[GAINS_SIZE] ={
+    GainInfo(80, GAIN_10X, GAIN_8X),     // 80x
+    GainInfo(20, GAIN_5X, GAIN_4X),      // 20x
+    GainInfo(256, GAIN_16X, GAIN_16X),   // 256x
+    GainInfo(5, GAIN_5X, GAIN_1X),       // 5x
+    GainInfo(50, GAIN_10X, GAIN_5X),     // 50x
+    GainInfo(128, GAIN_16X, GAIN_8X),    // 128x
+    GainInfo(512, GAIN_32X, GAIN_16X),   // 512x
+    GainInfo(2, GAIN_2X, GAIN_1X),       // 2x
+    GainInfo(10, GAIN_5X, GAIN_2X),      // 10x
+    GainInfo(32, GAIN_8X, GAIN_4X),      // 32x
+    GainInfo(64, GAIN_8X, GAIN_8X),      // 64x
+    GainInfo(100, GAIN_10X, GAIN_10X),   // 100x
+    GainInfo(160, GAIN_16X, GAIN_10X),   // 160x
+    GainInfo(320, GAIN_32X, GAIN_10X),   // 320x
+    GainInfo(1024, GAIN_32X, GAIN_32X),  // 1024x
+    GainInfo(1, GAIN_1X, GAIN_1X),       // 1x
+    GainInfo(4, GAIN_2X, GAIN_2X),       // 4x
+    GainInfo(8, GAIN_4X, GAIN_2X),       // 8x
+    GainInfo(16, GAIN_4X, GAIN_4X),      // 16x
+    GainInfo(25, GAIN_5X, GAIN_5X),      // 25x
+    GainInfo(40, GAIN_8X, GAIN_5X)       // 40x
+};
+
+
 /** @brief Measures the amplitude of the signal.
  *  Dynamically adjusts the gain to maintain the signal within a specified range.
  */
 void LoopAntenna::measureAmplitude(){
     amp1->changeChannel(amp1Ch);
     amp2->changeChannel(amp2Ch);
+
+    amp1->setGain(GAIN_1X);
+    amp2->setGain(GAIN_1X);
+    totalGain = 1;
 
     above = false;
 
@@ -42,10 +74,10 @@ void LoopAntenna::measureAmplitude(){
         if(amplitude < MINIMUM){
 
             // IF THE WAVE HAS FLATTENED AMPLITUDE IS 0
-            // if(isFlat()){
-            //     amplitude = 0;
-            //     return amplitude;
-            // }
+            if(isFlat()){
+                amplitude = 0;
+                return;
+            }
             
             // CALCULATE DESIRED GAIN
             totalGain = (TARGET / amplitude) * totalGain;
--- LoopAntenna.h
+++ LoopAntenna.h
@@ -7,7 +7,7 @@
 #define TARGET 700
 #define MINIMUM 350
 #define MAXIMUM 900
-#define FLAT 1
+#define FLAT 4
 #define SAMPLES 200
 #define GAINS_SIZE 21
 #define TREE_SIZE 31
@@ -34,27 +34,7 @@ private:
     int totalGain;
     int mean;
     int readings[SAMPLES];
-    static GainInfo tree[GAINS_SIZE] = {GainInfo(80, GAIN_10X, GAIN_8X),     // 80x
-                                 GainInfo(20, GAIN_5X, GAIN_4X),      // 20x
-                                 GainInfo(256, GAIN_16X, GAIN_16X),   // 256x
-                                 GainInfo(5, GAIN_5X, GAIN_1X),       // 5x
-                                 GainInfo(50, GAIN_10X, GAIN_5X),     // 50x
-                                 GainInfo(128, GAIN_16X, GAIN_8X),    // 128x
-                                 GainInfo(512, GAIN_32X, GAIN_16X),   // 512x
-                                 GainInfo(2, GAIN_2X, GAIN_1X),       // 2x
-                                 GainInfo(10, GAIN_5X, GAIN_2X),      // 10x
-                                 GainInfo(32, GAIN_8X, GAIN_4X),      // 32x
-                                 GainInfo(64, GAIN_8X, GAIN_8X),      // 64x
-                                 GainInfo(100, GAIN_10X, GAIN_10X),   // 100x
-                                 GainInfo(160, GAIN_16X, GAIN_10X),   // 160x
-                                 GainInfo(320, GAIN_32X, GAIN_10X),   // 320x
-                                 GainInfo(1024, GAIN_32X, GAIN_32X),  // 1024x
-                                 GainInfo(1, GAIN_1X, GAIN_1X),       // 1x
-                                 GainInfo(4, GAIN_2X, GAIN_2X),       // 4x
-                                 GainInfo(8, GAIN_4X, GAIN_2X),       // 8x
-                                 GainInfo(16, GAIN_4X, GAIN_4X),      // 16x
-                                 GainInfo(25, GAIN_5X, GAIN_5X),      // 25x
-                                 GainInfo(40, GAIN_8X, GAIN_5X)};     // 40x
+    static GainInfo tree[GAINS_SIZE];
  
 public: 
     LoopAntenna(MCP6S2x* ampA, MCP6S2x* ampB, 
